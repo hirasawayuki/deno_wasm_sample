ba33b19a72c7dc8733bf9eb6fdd33304cd782386
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-10-24 18:04:48 +0900

    Fix main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,22 @@
 import {PROBLEM_SET, solveJs, displayNumberPlace} from "./deps.ts";
-import { solve as solveWasm } from "./pkg/number_place.js";
+import { solve } from "./pkg/number_place.js";
 
 for (let p in PROBLEM_SET) {
-  let problem = PROBLEM_SET[p];
+  let problem: Uint8Array = PROBLEM_SET[p];
   console.log("============== problem ============")
-  // displayNumberPlace(problem);
-  {
-    console.log("============== result js ============")
-    const startTime = Date.now();
-    let result = solveJs(problem);
-    const endTime = Date.now();
+  displayNumberPlace(problem);
+  console.log("============== deno ============")
+  const startTime = Date.now();
+  let result = solveJs(problem);
+  const endTime = Date.now();
 
-    // displayNumberPlace(result);
-    console.log(`solve time: ${endTime - startTime}[ms]`);
-  }
+  displayNumberPlace(result);
+  console.log(`solve time: ${endTime - startTime}[ms]`);
 
-  {
-    console.log("============== result wasm ============")
-    const startTimeWasm = Date.now();
-    let resultWasm = solveWasm(problem);
-    const endTimeWasm = Date.now();
-    // displayNumberPlace(resultWasm);
-    console.log(`solve time: ${endTimeWasm - startTimeWasm}[ms]`);
-  }
+  console.log("============== wasm ============")
+  const startTimeWasm = Date.now();
+  let resultWasm = solve(problem);
+  const endTimeWasm = Date.now();
+  displayNumberPlace(resultWasm);
+  console.log(`solve time: ${endTimeWasm - startTimeWasm}[ms]`);
 }
